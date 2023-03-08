0e53e6a9204d7edbdde72cccd502767a0be024f2
Samuellekpor <samuellekpor@gmail.com>
2023-03-08 23:55:17 +0000

    Correct linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :name, length: {minimum: 1}
+  validates :name, length: { minimum: 1 }
 
   has_many :groups, dependent: :destroy
   has_many :activities, dependent: :destroy
