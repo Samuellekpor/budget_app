4445462934dcb92ef3c94e30b2619c5a835ebe91
Samuellekpor <samuellekpor@gmail.com>
2023-03-09 00:48:09 +0000

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get 'groups/index'
-  get 'groups/new'
+  # get 'groups/index'
+  # get 'groups/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "groups#index"
+  resources :users
+  resources :groups, only: [:index, :show, :new, :create, :destroy] do
+    resources :activities, only: [:index, :show, :new, :create, :destroy]
+  end
 end
