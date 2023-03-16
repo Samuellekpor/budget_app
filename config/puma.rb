c03c5d5565b1681d727662bd8f2cff69fc0a2f61
Samuellekpor <samuellekpor@gmail.com>
2023-03-16 14:31:29 +0000

    Made app listen on 0.0.0.0

--- config/puma.rb
+++ config/puma.rb
@@ -42,4 +42,4 @@ workers ENV.fetch("WEB_CONCURRENCY") { 4 }
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
-bind 'tcp://0.0.0.0:3000'
+bind "tcp://0.0.0.0:#{ENV.fetch("PORT") { 3000 }}"
